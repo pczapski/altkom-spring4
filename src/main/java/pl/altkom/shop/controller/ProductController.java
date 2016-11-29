05e73986e65e01c14d1ac2a1f7b2543b79595c82
MirekSz <m.szajowski@gmail.com>
2016-11-29 14:54:53 +0100

    lab06

---
+++ src/main/java/pl/altkom/shop/controller/ProductController.java
@@ -0,0 +1,18 @@
+package pl.altkom.shop.controller;
+
+import java.io.Writer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+	@RequestMapping("/list")
+	public void list(Writer writer, @RequestParam(required = false, value = "page") Integer page) throws Exception {
+		writer.write("Page: " + page);
+	}
+
+}
