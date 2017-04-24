7363066b4a09390d3f576616d506a481fba8c786
MirekSz <m.szajowski@gmail.com>
2017-04-24 10:09:18 +0200

    cxf

---
+++ src/main/java/pl/altkom/shop/controller/SaleDocumentRESTWebService.java
@@ -0,0 +1,25 @@
+package pl.altkom.shop.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.stereotype.Component;
+
+import pl.altkom.shop.model.SaleDocument;
+
+@Component
+@Path("/saleDocument")
+public class SaleDocumentRESTWebService {
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public SaleDocument findById(@PathParam("id") Long id) {
+		SaleDocument saleDocument = new SaleDocument();
+		saleDocument.setNumber("REST " + id);
+		return saleDocument;
+	}
+
+}
--- src/main/java/pl/altkom/shop/www/CXFConfig.java
+++ src/main/java/pl/altkom/shop/www/CXFConfig.java
@@ -1,29 +1,70 @@
 package pl.altkom.shop.www;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import javax.xml.ws.Endpoint;
+import javax.jws.WebService;
+import javax.ws.rs.Path;
 
 import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.feature.LoggingFeature;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 import pl.altkom.shop.controller.ProductSOAPWebService;
 
 @Configuration
 @ImportResource({ "classpath:META-INF/cxf/cxf.xml" })
-public class CXFConfig {
+public class CXFConfig implements ApplicationContextAware {
 	@Inject
 	private Bus bus;
 	@Inject
 	ProductSOAPWebService productSOAPWebService;
+	private ApplicationContext context;
+
+	@PostConstruct
+	public void start() {
+		bus.setFeatures(Arrays.asList(new LoggingFeature()));
+
+		String[] beanNamesForAnnotation = context.getBeanNamesForAnnotation(WebService.class);
+		for (String string : beanNamesForAnnotation) {
+			Object bean = context.getBean(string);
+			EndpointImpl endpoint = new EndpointImpl(bus, bean);
+			endpoint.publish("/" + bean.getClass().getSimpleName());
+		}
+	}
 
 	@Bean
-	public Endpoint productSOAP() {
-		EndpointImpl endpoint = new EndpointImpl(bus, productSOAPWebService);
-		endpoint.publish("/product");
-		return endpoint;
+	public Server rsServer() {
+		String[] beanNamesForAnnotation = context.getBeanNamesForAnnotation(Path.class);
+		JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
+		endpoint.setBus(bus);
+		endpoint.setAddress("/rest");
+		endpoint.setProvider(new JacksonJsonProvider());
+		List<Object> webServices = new ArrayList<>();
+		for (String string : beanNamesForAnnotation) {
+			webServices.add(context.getBean(string));
+		}
+		endpoint.setServiceBeans(webServices);
+		return endpoint.create();
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.context = applicationContext;
+
 	}
 
 }
