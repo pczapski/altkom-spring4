4312094435b17cb372f074c9a3295f64b28af64c
MirekSz <m.szajowski@gmail.com>
2017-05-02 10:39:13 +0200

    pre

--- src/main/java/pl/altkom/shop/service/Item.java
+++ src/main/java/pl/altkom/shop/service/Item.java
@@ -2,10 +2,8 @@ package pl.altkom.shop.service;
 
 public class Item {
 	public Item(long idProduct, int quantity) {
-		idProduct = l;
-		quantity = i;
 	}
 
-	public Long idProduct;
+	private Long idProduct;
 	public Integer quantity;
 }
