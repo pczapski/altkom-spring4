8202930f647418d72dacb3d0b4214fadba579ee1
MirekSz <m.szajowski@gmail.com>
2017-05-05 14:43:15 +0200

    logout

--- src/main/java/pl/altkom/shop/www/SecurityConfig.java
+++ src/main/java/pl/altkom/shop/www/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -25,7 +26,12 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	@Bean
 	@Override
 	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+		return new RegisterSessionAuthenticationStrategy(buildSessionRegistry());
+	}
+
+	@Bean
+	protected SessionRegistry buildSessionRegistry() {
+		return new SessionRegistryImpl();
 	}
 
 	@Autowired
@@ -36,7 +42,7 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		super.configure(http);
-		http.authorizeRequests().antMatchers("/product/**").hasAnyRole("USER", "ADMIN", "REST").anyRequest()
-				.permitAll();
+		http.authorizeRequests().antMatchers("/product/**").hasAnyRole("USER", "ADMIN", "REST").anyRequest().permitAll()
+				.and().logout().logoutUrl("/logout");
 	}
 }
