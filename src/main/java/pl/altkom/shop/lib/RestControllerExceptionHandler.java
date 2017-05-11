cbbe36d8a25895c40bbba6ccdde26ca6a3b9886d
MirekSz <m.szajowski@gmail.com>
2017-05-11 13:54:59 +0200

    ex

---
+++ src/main/java/pl/altkom/shop/lib/RestControllerExceptionHandler.java
@@ -0,0 +1,39 @@
+package pl.altkom.shop.lib;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice(annotations = RestController.class)
+public class RestControllerExceptionHandler {
+	public static class ServerError {
+		private String result = "error :-(";
+		private String error;
+
+		public ServerError(String error) {
+			this.setError(error);
+		}
+
+		public String getError() {
+			return error;
+		}
+
+		public void setError(String error) {
+			this.error = error;
+		}
+
+		public String getResult() {
+			return result;
+		}
+
+	}
+
+	@ResponseBody
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ServerError> handle(final Exception exception) {
+		return ResponseEntity.ok(new ServerError(ExceptionUtils.getStackTrace(exception)));
+	}
+}
