ff944b9375fd6f444fffae75bbede2e10d83194b
MirekSz <m.szajowski@gmail.com>
2017-05-15 15:16:38 +0200

    reloadable msg

--- src/main/java/pl/altkom/shop/www/WebConfig.java
+++ src/main/java/pl/altkom/shop/www/WebConfig.java
@@ -1,10 +1,9 @@
 package pl.altkom.shop.www;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -54,12 +53,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	}
 
 	@Bean
-	public MessageSource messageSource() {
-		ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
-		resourceBundleMessageSource.setBasename("messages");
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource resourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+		resourceBundleMessageSource.setBasename("classpath:messages");
+		resourceBundleMessageSource.setCacheSeconds(1);
 		return resourceBundleMessageSource;
 	}
-	
+
 	@Bean
 	public CommonsMultipartResolver multipartResolver() {
 		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
