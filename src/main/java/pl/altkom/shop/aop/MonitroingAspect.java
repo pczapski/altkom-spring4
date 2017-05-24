0ef1316d5198fb61479d9bd5d30e129d2976773e
Student <Student@10.10.22.7>
2017-05-24 16:15:09 +0200

    method delegate

--- src/main/java/pl/altkom/shop/aop/MonitroingAspect.java
+++ src/main/java/pl/altkom/shop/aop/MonitroingAspect.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
@@ -14,21 +15,38 @@ import org.springframework.stereotype.Component;
 public class MonitroingAspect {
 	Logger log = Logger.getLogger(MonitroingAspect.class);
 
-	@Around("@annotation(pl.altkom.shop.aop.Monitoring)")
+	public static boolean ENABLED = false;
+
+	@Pointcut("@within(org.springframework.stereotype.Service)")
+	public void allServices() {
+	}
+
+	@Pointcut("@within(org.springframework.stereotype.Repository)")
+	public void allRepos() {
+	}
+
+	@Around("@annotation(pl.altkom.shop.aop.Monitoring) || allRepos() || allServices()")
 	public Object monitpr(ProceedingJoinPoint pjp) throws Throwable {
-		MethodSignature ms = (MethodSignature) pjp.getSignature();
-		Method m = ms.getMethod();
-		long max = m.getAnnotation(Monitoring.class).maxTime();
-		long currentTimeMillis = System.currentTimeMillis();
-		Object obj = pjp.proceed();
-		long end = System.currentTimeMillis() - currentTimeMillis;
-		log.info(m + " took: " + end);
-		if (end > max) {
-			log.error(m + " took: " + end);
-		} else {
+		if (ENABLED) {
+			MethodSignature ms = (MethodSignature) pjp.getSignature();
+			Method m = ms.getMethod();
+
+			Object target = pjp.getTarget();
+			Method declaredMethod = target.getClass().getDeclaredMethod(m.getName());
+
+			long max = declaredMethod.getAnnotation(Monitoring.class).maxTime();
+			long currentTimeMillis = System.currentTimeMillis();
+			Object obj = pjp.proceed();
+			long end = System.currentTimeMillis() - currentTimeMillis;
 			log.info(m + " took: " + end);
+			if (end > max) {
+				log.error(m + " took: " + end);
+			} else {
+				log.info(m + " took: " + end);
+			}
+			return obj;
 		}
-		return obj;
+		return pjp.proceed();
 	}
 
 }
